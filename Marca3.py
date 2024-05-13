080ff15424ea74f1459ea186e120f59c5b284116
PedroDeCabo <pedro.decabo@tajamar365.com>
2024-05-13 21:18:29 +0200

    Franco

---
+++ Marca3.py
@@ -0,0 +1,7 @@
+def Saludar(Nombre):
+    print("Hola", Nombre, ",Encantado de conocerte.")
+def Despedir(Nombre):
+    print("Ahora, vete a tomar por culo:", Nombre, ".")
+Nombre = input("Digame su nombre: ")
+Saludar(Nombre)
+Despedir(Nombre)

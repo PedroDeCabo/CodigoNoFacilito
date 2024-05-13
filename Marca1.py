9c16c8002f867c30ea10867c3239ef90534245f4
PedroDeCabo <pedro.decabo@tajamar365.com>
2024-05-13 17:31:12 +0200

    Ej

--- Marca1.py
+++ Marca1.py
@@ -5,3 +5,12 @@ def potencias(a, b):
     return a ** b
 print(sumar(2, 2))
 print(potencias(2, 2))
+class Ejemplo:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self):
+        print(f"Hola, {self.nombre}!")
+
+ejemplo = Ejemplo("Juan")
+ejemplo.saludar()
